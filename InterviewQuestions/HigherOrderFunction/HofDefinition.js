39d9021461b9786a6de075736d4c82e44fa396f4
vikas7974 <vikasmenghani12@gmail.com>
2024-10-22 21:52:13 +0530

    day2_HOF

---
+++ InterviewQuestions/HigherOrderFunction/HofDefinition.js
@@ -0,0 +1,12 @@
+
+//                       Definition
+
+//  Higher - order functions in JavaScript are functions that can take
+//  other functions as arguments, return functions as their result.
+
+//  Functions as First-Class Citizens: In JavaScript, functions are treated as
+//  first-class citizens,meaning they can be assigned to variables, passed as arguments,
+//  and returned from other functions
+
+
+// Examples : Callback, Function Composition and Currying
