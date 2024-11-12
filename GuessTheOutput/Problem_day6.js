461eb4fc93cf276b7be1a5403961861a2fb510ca
vikas7974 <vikasmenghani12@gmail.com>
2024-11-12 20:25:36 +0530

    Prblm1_day6

---
+++ GuessTheOutput/Problem_day6.js
@@ -0,0 +1,37 @@
+// Guess The Output of the below js code
+
+//----------------------------------------------------------------
+
+// Code fragment
+
+var a = 10;
+
+function first() {
+    console.log(a);  
+    var a = 20;
+    console.log(a); 
+}
+
+first();
+console.log(a);  
+
+// Options
+
+// a) undefined, 20, 10
+// b) undefined, 20, 20
+// c) 10, 20, 10
+// d) undefined, undefined, 10
+
+
+//---------------------------------------------------------------
+
+
+// Output
+
+
+// a) undefined, 20, 10
+
+
+
+//----------------------------------------------------------------
+
