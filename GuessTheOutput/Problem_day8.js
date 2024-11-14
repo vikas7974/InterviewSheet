0702f088490125f56e63eec62d439acf803a4120
vikas7974 <vikasmenghani12@gmail.com>
2024-11-14 23:03:11 +0530

    Prblm_day8

---
+++ GuessTheOutput/Problem_day8.js
@@ -0,0 +1,40 @@
+// Guess The Output of the below js code
+
+//----------------------------------------------------------------
+
+// Code fragment
+
+
+
+console.log(0 == false);     
+console.log(0 === false);    
+console.log('5' - 1);        
+console.log('5' + 1);        
+console.log([] == false);    
+console.log([] === false);   
+console.log(null == undefined); 
+console.log(null === undefined); 
+
+
+
+
+//---------------------------------------------------------------
+
+
+// Output
+
+
+// true
+// false
+// 4
+// 51
+// true
+// false
+// true
+// false
+
+
+
+//----------------------------------------------------------------
+
+
