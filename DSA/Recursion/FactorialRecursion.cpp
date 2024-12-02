89ed937b677e4af3413c81c0fbf7ec2e7c96175e
vikas7974 <vikasmenghani12@gmail.com>
2024-12-02 22:52:27 +0530

    DSA_day2

---
+++ DSA/Recursion/FactorialRecursion.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int fact(int N)
+{
+    if (N == 1)
+        return 1;
+    return N * fact(N - 1);
+}
+
+int main()
+{
+
+    cout << fact(5) << endl;
+
+    return 0;
+}
