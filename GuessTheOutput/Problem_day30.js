649560e654befa104e55117a737e13c90ac2b704
vikas7974 <vikasmenghani12@gmail.com>
2025-01-01 23:51:07 +0530

    Prblm_day30_main

---
+++ GuessTheOutput/Problem_day30.js
@@ -0,0 +1,33 @@
+// Guess The Output of the below js code
+
+//----------------------------------------------------------------
+
+// Code fragment
+
+
+(function () {
+    var a = b = 10;
+})();
+
+console.log(typeof a); // What is the output?
+console.log(typeof b); // What is the output?
+
+
+
+
+
+
+
+
+//---------------------------------------------------------------
+
+
+// Output
+
+// undefined, number
+
+
+
+
+
+//----------------------------------------------------------------
